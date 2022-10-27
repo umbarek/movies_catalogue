9ba9374ced564beac8d10c1cc8be969c500dc2e2
Tomasz Pograniczny <tpograniczny@gmail.com>
2022-10-27 20:50:09 +0200

    URL, film list

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import tmdb_client
+import __main__
 
 app = Flask(__name__)
 
+type_list = ["popular", "now_playing", "top_rated", "upcoming"]
+
 @app.route('/')
 def homepage():
-    movies = []
-    return render_template("homepage.html", movies=movies)
+    selected_list = request.args.get("list_type", "popular")
+    if selected_list not in type_list:
+        selected_list = "popular"
+    movies = tmdb_client.get_movies(how_many=8, list_type=selected_list)
 
-if __name__ == '__main__':
+    return render_template("homepage.html",
+                            movies=movies,
+                            current_list=selected_list,
+                            new_list=type_list)
+
+@app.context_processor
+def utility_processor():
+    def tmdb_image_url(path, size):
+        return tmdb_client.get_poster_url(path, size)
+
+    return {"tmdb_image_url": tmdb_image_url}
+
+if __name__ == "__main__":
     app.run(debug=True)
---
+++ tmdb_client.py
@@ -0,0 +1,24 @@
+import requests
+
+api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzODc0MTcxYWIyZjkyNTFjOTZhZmVlYWI1OTAzNDEwOSIsInN1YiI6IjYzNTk5MTg3ZTg5NGE2MDA3ZTI0MTZiYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BqmwYHluaW2UFrHDlQ99mSKIm_t2V43Z87fho9roNB4"
+endpoint = "https://api.themoviedb.org/3/movie/"
+headers = {"Authorization": f"Bearer {api_token}"}
+
+def get_popular_movies():
+    response = requests.get(endpoint+f"popular", headers=headers)
+    return response.json()
+
+
+def get_poster_url(poster_api_path, size="w342"):
+    base_url = "https://image.tmdb.org/t/p/"
+    return f"{base_url}{size}/{poster_api_path}"
+
+
+def get_movies(how_many, list_type):
+    data = get_movies_list(list_type)
+    return data["results"][:how_many]
+
+def get_movies_list(list_type):
+    response = requests.get(endpoint+f"{list_type}", headers=headers)
+    response.raise_for_status()
+    return response.json()
